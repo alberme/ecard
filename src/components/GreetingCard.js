e927374aa59dc1fa1ab0049a5b0a827154c9896a
alberme <albertmartinez16@gmail.com>
2021-12-15 02:32:23 -0800

    refactor(card render): remove header

--- src/components/GreetingCard.js
+++ src/components/GreetingCard.js
@@ -1,20 +1,19 @@
+import { useState } from 'react';
 import snow from '../images/snow.jpg';
+
 
 function GreetingCard({ greeting, body, closing, setCustomize }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={snow} className="App-logo" alt="snow" />
-        <p>{greeting}</p>
-        <p>{body}</p>
-        <p>{closing}</p>
-        <button
-          className="App-link"
-          onClick={() => setCustomize(true)}
-        >
-          Reshare This Card
-        </button>
-      </header>
+    <div className="App-header">
+      <img src={snow} className="App-logo" alt="snow" />
+      <p>{greeting}</p>
+      <p>{body}</p>
+      <p>{closing}</p>
+      <button
+        onClick={() => setCustomize(true)}
+      >
+        Reshare This Card
+      </button>
     </div>
   );
 }
