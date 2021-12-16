3357ecb7a2be993a200023447e8b7d28f123b6ef
alberme <albertmartinez16@gmail.com>
2021-12-16 00:50:44 -0800

    refactor(preview): move preview logic from App to Preview

---
+++ src/components/Preview/index.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import GreetingCard from '../GreetingCard';
+import Share from '../Share';
+import './styles.css';
+
+export default function Preview ({ greetingData, setStatus }) {
+  const [showShare, setShowShare] = useState(false);
+  
+  return (
+    <div className="preview-container">
+      <GreetingCard
+        greetingData={greetingData}
+        setStatus={setStatus}
+      />
+      <Share
+        show={showShare}
+        setShowShare={setShowShare}
+        greetingData={greetingData}
+      />
+      <div className="preview-buttons">
+        <button
+          onClick={() => setShowShare(true)}
+        >
+          Share Greeting
+        </button>
+        <button
+          onClick={() => setStatus('customize')}
+        >
+          Customize
+        </button>
+      </div>
+    </div>
+  )
+}
